c2f78e1334a067b7d7f7f94e78bb20bc293aa60a
Maike Souza Leite <maikesouzaleite@gmail.com>
2025-07-05 02:53:56 -0300

    feat: implementar navegação com CustomTabBar e atualizar telas de tabulação

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,12 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Profile() {
   return (
-    <View>
+    <ScreenWrapper>
       <Text>Profile</Text>
-    </View>
+    </ScreenWrapper>
   );
 }
 
